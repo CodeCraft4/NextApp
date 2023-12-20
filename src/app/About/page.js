aaeb157cdb65afdc0090b1a24f9338acb2534d70
ImadSha <imadshah4104@gmail.com>
2023-12-20 02:05:48 -0700

    .

--- src/app/About/page.js
+++
@@ -1,7 +0,0 @@
-const about=()=>{
-    return(
-        <div>
-            <h2>About page button</h2>
-        </div>
-    )
-}
---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './globals.css'
+
+const page = () => {
+  return (
+    <React.Fragment>
+       <h1>Hellow how are you</h1>
+    </React.Fragment>
+  )
+}
+
+export default page
